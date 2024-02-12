61d2e7076ac675318c08cb410dc496b563be0aa9
isaiah <isaiahmartin847@gmail.com>
2024-02-12 13:35:46 -0700

    added a window watcher to log if window is moble size or not

--- tracker/tracks/static/js/main.js
+++ tracker/tracks/static/js/main.js
@@ -6,3 +6,12 @@ function idTracks() {
     console.log("button was clicked");
     window.location.href = 'tracks';
 }
+
+
+window.addEventListener('resize', function() {
+    if (this.window.innerWidth < 700){
+        console.log("mobel response")
+    }
+})
+
+
